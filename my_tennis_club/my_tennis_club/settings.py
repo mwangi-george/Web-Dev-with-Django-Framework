59f43f8c9efae78126fee49de6325a1abec5ec5f
mwangi-george <mwangigeorge648@gmail.com>
2024-04-25 10:16:23 +0300

    added Postgres DB and Requirements file

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'masteruser',
+        'HOST': 'w3-django-project.cjugm2ea821x.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
